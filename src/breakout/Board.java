4808dbb4806bb1be96c14da2fa9c405ec5ffc843
Joe Cutting <Joe Cutting>
2015-06-17 12:59:31 +0100

    fix for last commit

--- src/breakout/Board.java
+++ src/breakout/Board.java
@@ -97,6 +97,7 @@ public class Board extends JPanel implements Commons {
     }
     private void startGame() {
         waitToStart=false;
+        ingame=true;
 
 
     }
@@ -183,7 +184,7 @@ public class Board extends JPanel implements Commons {
     class ScheduleTask extends TimerTask {
 
         public void run() {
-            if (!waitToStart) {
+            //if (!waitToStart) {
                 hasBounced = ball.move();
                 controller.increaseTicks();
                 int dx = controller.getAction(thisBoard);
@@ -203,10 +204,10 @@ public class Board extends JPanel implements Commons {
                 if (restartGame) {
                     restartGame();
                 }
-            } else {
-                repaint();
+         //   } else {
+           //     repaint();
 
-            }
+            //}
         }
     }
 
